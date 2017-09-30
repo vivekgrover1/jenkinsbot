3cb1d9483240f5eaec396051d45b80a78a35e787
vivekgrover1 <vivek271091@gmail.com>
2017-09-30 22:04:46 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -3,4 +3,3 @@ create user 'jenkinsbot'@'localhost' identified by 'jenkinsbot';
 grant all on jenkinsbotdb.* to 'jenkinsbot' identified by 'jenkinsbot';
 use jenkinsbotdb;
 create table jenkinsbot_job_status ( username varchar(30), job_id_1 varchar(20), job_id_2 varchar(20),job_id_3 varchar(20),PRIMARY KEY(username));
-insert into jenkinsbot_job_status values('Vivek271091','Not Approved','Not Approved','Not Approved');
