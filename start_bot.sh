8142fac11a0b504614e13260517a17ce9b877392
vivekgrover1 <vivek271091@gmail.com>
2017-10-05 17:33:42 +0530

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-echo "HI"
 service mysql start
-sleep 20
+sleep 10
 mysql -h "localhost" -u "root" "-psecretadmin" < "/docker-entrypoint-initdb.d/init.sql"
 sleep 5
 nohup python3.4 /root/bot/slackbot.py & 2>&1 >/dev/null
-
-#python3.4 /root/bot/slackbot.py 
 nohup python3.4 /root/bot/start_app.py & 2>&1 >/dev/null
-sleep 500
-echo "Hi"
-
+pid1=`ps -ef | grep py | grep -v grep | grep "/root/bot/slackbot.py" | awk '{ print $2 }'`
+pid2=`ps -ef | grep py | grep -v grep | grep "/root/bot/start_app.py" | awk '{ print $2 }'`
+while [ ! -z "$pid1" && ! -z "$pid2" ]
+do
+sleep 200
+pid1=`ps -ef | grep py | grep -v grep | grep "/root/bot/slackbot.py" | awk '{ print $2 }'`
+pid2=`ps -ef | grep py | grep -v grep | grep "/root/bot/start_app.py" | awk '{ print $2 }'`
+done
