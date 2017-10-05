b51303b5f864167238ff0bd62dbdbf9cfc9c9a53
vivekgrover1 <vivek271091@gmail.com>
2017-10-05 18:23:07 +0530

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -7,11 +7,11 @@ nohup python3.4 /root/bot/slackbot.py & 2>&1 >/dev/null
 nohup python3.4 /root/bot/start_app.py & 2>&1 >/dev/null
 pid1=`ps -ef | grep py | grep -v grep | grep "/root/bot/slackbot.py" | awk '{ print $2 }'`
 pid2=`ps -ef | grep py | grep -v grep | grep "/root/bot/start_app.py" | awk '{ print $2 }'`
-pid3=`ps -ef | grep mysql | grep -v grep | grep /usr/bin/mysqld_safe | awk '{ print $2 }'`
+pid3=`ps -ef | grep mysql | grep -v grep | grep mysqld | awk '{ print $2 }'`
 while [ ! -z "$pid1" -a ! -z "$pid2" -a ! -z "$pid3" ]
 do
 sleep 200
 pid1=`ps -ef | grep py | grep -v grep | grep "/root/bot/slackbot.py" | awk '{ print $2 }'`
 pid2=`ps -ef | grep py | grep -v grep | grep "/root/bot/start_app.py" | awk '{ print $2 }'`
-pid3=`ps -ef | grep mysql | grep -v grep | grep /usr/bin/mysqld_safe | awk '{ print $2 }'`
+pid3=`ps -ef | grep mysql | grep -v grep | grep mysqld | awk '{ print $2 }'`
 done
