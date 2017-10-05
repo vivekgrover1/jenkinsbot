8e076c2920318c02f7c38d31fdeeab1651888315
vivekgrover1 <vivek271091@gmail.com>
2017-10-05 13:11:20 +0530

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "HI"
-#sleep 50
+sleep 50
 #nohup python3.4 /root/bot/slackbot.py & 2>&1 >/dev/null
-python3.4 slackbot.py 
+python3.4 /root/bot/slackbot.py 
 #nohup python3.4 /root/bot/start_app.py & 2>&1 >/dev/null
 #sleep 500
 echo "Hi"
