48d6fbd5916e4fb163b09c5287761826b2185de6
vivekgrover1 <vivek271091@gmail.com>
2017-10-05 16:16:23 +0530

    Update start_bot.sh

--- start_bot.sh
+++ start_bot.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 echo "HI"
 service mysql start
-sleep 50
+sleep 20
+mysql -h "localhost" -u "root" "-psecretadmin" < "/docker-entrypoint-initdb.d/init.sql"
 #nohup python3.4 /root/bot/slackbot.py & 2>&1 >/dev/null
+sleep 5
 python3.4 /root/bot/slackbot.py 
 #nohup python3.4 /root/bot/start_app.py & 2>&1 >/dev/null
 #sleep 500
