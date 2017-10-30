14dff9f2558efa8f5afbaef7b624b8ab81d2bc4a
vivekgrover1 <vivek271091@gmail.com>
2017-10-30 19:38:48 +0530

    Update slackbot.py

--- slackbot.py
+++ slackbot.py
@@ -54,9 +54,9 @@ def handle_command(command, channel, msg_id, user_id):
         message to slack thread or message.
     """
     username = get_user_name(user_id, slack_client)
-    #value = python_mysql.get_status(username)
-    #if value == None:
-     # python_mysql.add_user(username)
+    value = python_mysql.get_status(username)
+    if value == None:
+      python_mysql.add_user(username)
 
     if command == "member joined":
         msg = ":slack: Welcome to the channel, Here you can instruct the jenkinsbot to execute the job based on the " \
